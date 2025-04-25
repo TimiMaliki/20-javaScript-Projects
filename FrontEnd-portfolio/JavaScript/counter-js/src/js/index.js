f869077c0acbcb5fd4cb7bdd6ebaec730acfc121
TimiMaliki <malikitimi@gmail.com>
2025-04-25 17:43:09 +0100

    counter

---
+++ FrontEnd-portfolio/JavaScript/counter-js/src/js/index.js
@@ -0,0 +1,34 @@
+// const increment = document.querySelector('.increase-btn')
+// const decrement = document.querySelector('.decrease-btn')
+// const reset = document.querySelector('.reset-btn')
+// const number = document.querySelector('.number')
+
+// let value = 0
+
+// increment.addEventListener('click' , ()=>{
+//     number.style.color = "gold"
+//     const result = value++
+//    number.innerText = result
+//   console.log(value)
+// })
+
+// decrement.addEventListener('click' , ()=>{
+//     number.style.color = "pink"
+//     const result = value--
+//    number.innerText = result
+//   console.log(value)
+// })
+
+
+// reset.addEventListener('click' , ()=>{
+//     value = 0
+//     number.style.color = "black"
+//    number.innerText = value
+//   console.log(value)
+// })
+
+
+// OR
+
+
+const number = document.querySelector('.number')
