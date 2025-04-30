c4134df2c01511bff2f1feeb036d48f164b2b98c
TimiMaliki <malikitimi@gmail.com>
2025-04-30 22:57:29 +0100

    Done

--- FrontEnd-portfolio/JavaScript/sidebar/src/index.js
+++ FrontEnd-portfolio/JavaScript/sidebar/src/index.js
@@ -3,6 +3,19 @@ const closeBtn = document.querySelector('.close-btn')
 const links = document.querySelector('.sidebar')
 
 
+// toggle.addEventListener('click' , ()=>{
+//     if(links.classList.contains('show-sidebar')){
+//         links.classList.remove('show-sidebar')
+//     }else{
+//         links.classList.add('show-sidebar')
+//     }
+//    })
+
+
 toggle.addEventListener('click' , ()=>{
  links.classList.toggle('show-sidebar')
+})
+
+closeBtn.addEventListener('click' , ()=>{
+    links.classList.remove('show-sidebar')
 })
