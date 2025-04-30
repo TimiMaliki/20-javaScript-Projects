ccbec7f164794e5538be611e665d1bbd886e125a
TimiMaliki <malikitimi@gmail.com>
2025-05-01 00:39:02 +0100

    added : alternate method to js

--- FrontEnd-portfolio/JavaScript/QA/src/index.js
+++ FrontEnd-portfolio/JavaScript/QA/src/index.js
@@ -15,3 +15,15 @@ questions.forEach((question)=>{
     })
 
 })
+
+
+// traversing the dom
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+
+//     question.classList.toggle("show-text");
+//   });
+// });
