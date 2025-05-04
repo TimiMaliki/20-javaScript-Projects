0012175551d5c18b73739c7e30aa05e7147a943c
TimiMaliki <malikitimi@gmail.com>
2025-05-04 18:58:17 +0100

    added : navbar

--- FrontEnd-portfolio/JavaScript/scroll/src/index.js
+++ FrontEnd-portfolio/JavaScript/scroll/src/index.js
@@ -1,9 +1,15 @@
-
 // Footer getFullDate
 
-const date = document.querySelector('#date')
-date.innerText = new Date().getFullYear()
+const date = document.querySelector("#date");
+date.innerText = new Date().getFullYear();
 
 // toggle navbar
 
-const navbar = document.querySelector('.')
+const navbar = document.querySelector(".nav-toggle");
+const container = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navbar.addEventListener("click", () => {
+ container.classList.toggle('show-links')
+    console.log('hello')
+});
