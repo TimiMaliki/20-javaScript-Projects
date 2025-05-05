d70525be43b5ff21740b32303706d92cf11eb3f6
TimiMaliki <malikitimi@gmail.com>
2025-05-05 17:48:59 +0100

    Tabs

--- FrontEnd-portfolio/JavaScript/Tabs/src/index.js
+++ FrontEnd-portfolio/JavaScript/Tabs/src/index.js
@@ -1,9 +1,23 @@
-const btns = document.querySelectorAll('.tab-btn')
+const btns = document.querySelectorAll(".tab-btn");
+const content = document.querySelectorAll(".content");
+const about = document.querySelector(".about");
 
 
-btns.forEach((btn) => {
-  btn.addEventListener("click" , (e)=> {
-          const id = e.target.dataSet
-          console.log(id)
-  })
-})
+
+about.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+      console.log(btn);
+    });
+    content.forEach((article) => {
+      console.log(article);
+      article.classList.remove("active");
+    });
+
+    const el = document.getElementById(id);
+    el.classList.add("active");
+  }
+});
