daff356b02edc17f18d99842194a58e992d39a1c
TimiMaliki <malikitimi@gmail.com>
2025-05-06 03:13:07 +0100

    countDown

--- FrontEnd-portfolio/JavaScript/countDown/src/index.js
+++ FrontEnd-portfolio/JavaScript/countDown/src/index.js
@@ -3,9 +3,41 @@ const  hours = document.querySelector('.hours')
 const  mins = document.querySelector('.minutes')
 const  seconds = document.querySelector('.seconds')
 
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
 
-days.innerText = new Date().getDay()
-hours.innerText = new Date().getHours()
-mins.innerText = new Date().getMinutes()
-seconds.innerText = new Date().getSeconds()
+
+const giveaway = document.querySelector('.giveaway')
+
+const futureDate = new Date(2025, 4 , 5, 2, 39, 58 )
+const year = futureDate.getFullYear()
+const day = futureDate.getDay()
+const month =  futureDate.getMonth()
+const hour = futureDate.getHours()
+const min = futureDate.getMinutes()
+const sec = futureDate.getSeconds()
+
+
+giveaway.textContent = `giveaway ends on  ${weekdays[day]}, ${months[month]}, ${year},  ${hour}:${min}am`
 
