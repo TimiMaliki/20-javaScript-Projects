aca85e860b8a32fc26dc970d35fde63b6d297f40
TimiMaliki <malikitimi@gmail.com>
2025-05-10 15:07:36 +0100

    added : data

--- FrontEnd-portfolio/JavaScript/darkmode/src/data.js
+++ FrontEnd-portfolio/JavaScript/darkmode/src/data.js
@@ -2,31 +2,44 @@
 const articles = [
     {
       id: 1,
-      title: '',
-      date: new Date(2020, 9, 4),
+      title: 'Pope Leo signals he will closely follow Francis and says AI represents challenge for humanity',
+      date: new Date(2025, 4, 10),
       length: 11,
-      snippet: `I'm baby ramps kombucha gluten-free ennui swag tattooed street art. Marfa biodiesel letterpress blue bottle subway tile, pop-up pok pok of.`,
+      snippet: `
+      Rome
+      CNN
+       — 
+      Pope Leo XIV indicated on Saturday that his papacy will follow closely in the footsteps of the late Pope Francis, telling church cardinals that they should take up that “precious legacy” and identifying artificial intelligence as a main challenge for working people and “human dignity.”
+      
+      `,
     },
     {
       id: 2,
-      title: 'goodby, clean code',
-      date: new Date(2019, 10, 22),
+      title: 'How female bonobos team up to gain power over males',
+      date: new Date(2025, 4, 10),
       length: 5,
-      snippet: `Biodiesel artisan seitan plaid sriracha copper mug venmo shabby chic. Kickstarter raclette kombucha, yr post-ironic jianbing try-hard flexitarian vaporware normcore.`,
+      snippet: `Unusual among groups of social mammals, the female bonobo enjoys greater status than the male bonobo. Seeking to understand why, researchers observed six communities of bonobos in the Democratic Republic of the Congo for nearly three decades—and now they believe they might have the answer.`,
     },
     {
       id: 3,
-      title: 'my decade in review',
+      title: 'Nvidia modifies H20 chip for China to overcome US export controls, sources say',
       date: new Date(2018, 7, 11),
       length: 5,
-      snippet: `Direct trade shabby chic four dollar toast, tilde actually try-hard bicycle rights aesthetic forage. Meditation keytar asymmetrical tacos artisan truffaut. Pabst jean shorts roof party scenester.`,
+      snippet: `By Liam Mo and Brenda Goh
+
+      BEIJING (Reuters) -Nvidia (NVDA) plans to release a downgraded version of its H20 artificial intelligence chip for China in the next two months, following U.S. export restrictions on the original model, three sources familiar with the matter told Reuters.
+      
+      The U.S. chipmaker has notified major Chinese customers, including leading cloud computing providers, that it aims to release the modified H20 chip in July, two of the sources said.`,
     },
     {
       id: 4,
-      title: 'what are the react team principles',
+      title: `WE’RE ALL LIVING IN A CARL HIAASEN NOVEL
+      In the mangroves with Florida’s poet of excess and grift
+      
+      By Amy Weiss-Meyer`,
       date: new Date(2015, 5, 4),
       length: 5,
-      snippet: `Selvage street art hammock affogato VHS. Mustache shaman literally wayfarers schlitz. Direct trade four loko narwhal VHS pop-up, chartreuse trust fund typewriter street art swag thundercats art party.`,
+      snippet: `Nothing about Carl Hiaasen’s outward appearance suggests eccentricity. I’ve seen him described as having the air of “an amiable dentist” or “a pleasant jeweler” or “a patrician country lawyer.” He is soft-spoken, courteous, and plainly dressed. The mischief is mostly detectable in his eyes, which he’ll widen to express disbelief or judgment, or cast sideways to invite a companion to join him on his wavelength, raising his brows for effect.`,
     },
   ];
   
