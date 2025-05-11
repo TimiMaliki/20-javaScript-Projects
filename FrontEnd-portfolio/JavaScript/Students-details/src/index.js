dff854417c26ce67a6a0a88e0147c93b13615b9a
TimiMaliki <malikitimi@gmail.com>
2025-05-11 11:42:22 +0100

    js

--- FrontEnd-portfolio/JavaScript/Students-details/src/index.js
+++ FrontEnd-portfolio/JavaScript/Students-details/src/index.js
@@ -4,16 +4,39 @@ const nameOfStudent = document.getElementById('name')
 const roll = document.getElementById('roll')
 const studentDom = document.querySelector('.students')
 
-const students = []
-
+const students = [{
+   nameOfStudent : "yusuf",
+    age : 23 ,
+    roll : 4
+}]
+ 
 const displayRegisteredStudents = ({nameOfStudent , age , roll}) =>{
     const displayInnerHtml = `
     <ul class="list-items">
-    <li class="items">${nameOfStudent}</li>
-    <li class="items">${age}</li>
-    <li class="items">${roll}</li>
+    <li class="items">${nameOfStudent.value}</li>
+    <li class="items">${age.value}</li>
+    <li class="items">${roll.value}</li>
   </ul>
     `
-
-
+    studentDom.innerHTML = displayInnerHtml
 }
+
+const addStudents = ({nameOfStudent , age ,roll})=>{
+
+    const addUp = {
+        nameOfStudent,age,roll
+    }
+
+    students.push(addUp)
+}
+
+
+students.forEach(displayRegisteredStudents)
+
+console.log(students)
+
+form.addEventListener('submit' , (e)=>{
+  e.preventDefault()
+ 
+
+})
