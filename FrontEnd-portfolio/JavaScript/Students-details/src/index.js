00dae42d7d66ba06e7c9b6fc445929c72727f717
TimiMaliki <malikitimi@gmail.com>
2025-05-11 20:28:16 +0100

    added : local storage

--- FrontEnd-portfolio/JavaScript/Students-details/src/index.js
+++ FrontEnd-portfolio/JavaScript/Students-details/src/index.js
@@ -4,48 +4,46 @@ const nameOfStudent = document.getElementById("name");
 const roll = document.getElementById("roll");
 const studentDom = document.querySelector(".students");
 
-const students = [];
+const students = JSON.parse(localStorage.getItem("items")) || [];
 
-const displayRegisteredStudents = ({person, age, roll}) => {
-  const container = document.createElement("ul")
-  container.classList.add('list-items')
-  const studentName= document.createElement("li")
-  const studentAge= document.createElement("li")
-  const studentRoll= document.createElement("li")
-  container.classList.add('list-items')
-  studentName.classList.add('items')
-  studentAge.classList.add('items')
-  studentRoll.classList.add('items')
+const displayRegisteredStudents = ({ person, age, roll }) => {
+  const container = document.createElement("ul");
+  container.classList.add("list-items");
+  const studentName = document.createElement("li");
+  const studentAge = document.createElement("li");
+  const studentRoll = document.createElement("li");
+  container.classList.add("list-items");
+  studentName.classList.add("items");
+  studentAge.classList.add("items");
+  studentRoll.classList.add("items");
 
-   studentName.innerText = person
-   studentAge.innerText = age <= 0 ? "" : "error"
-   studentRoll.innerText = roll
+  studentName.innerText = person;
+  studentAge.innerText = age >= 1 ? age : "error(age is below One)";
+  studentRoll.innerText = roll;
 
-   container.append(studentName,studentAge,studentRoll)
-  studentDom.appendChild(container)
+  container.append(studentName, studentAge, studentRoll);
+  studentDom.appendChild(container);
 };
 
-const addStudents = (person, age, roll ) => {
+const addStudents = (person, age, roll) => {
   students.push({ person, age, roll });
-  return {person, age, roll};
+   localStorage.setItem("items" , JSON.stringify(students))
+  return { person, age, roll };
 };
-
-
 
 // console.log(students);
 
+students.forEach(displayRegisteredStudents)
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const addNewStudents = addStudents(
-    nameOfStudent.value ,
-    age.value,
-    roll.value
+  //   const addNewStudents =
+
+  displayRegisteredStudents(
+    addStudents(nameOfStudent.value, age.value, roll.value)
   );
 
-  displayRegisteredStudents(addNewStudents);
-
-
-  nameOfStudent.value = ''
-    age.value = ''
-    roll.value = ''
+  nameOfStudent.value = "";
+  age.value = "";
+  roll.value = "";
 });
