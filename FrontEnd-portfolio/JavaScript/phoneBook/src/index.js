2acb161ac32d68cf200943ddbee5adead6a1cff0
TimiMaliki <malikitimi@gmail.com>
2025-05-12 21:02:05 +0100

    phoneBook

--- FrontEnd-portfolio/JavaScript/phoneBook/src/index.js
+++ FrontEnd-portfolio/JavaScript/phoneBook/src/index.js
@@ -15,43 +15,43 @@ closeContactBtn.addEventListener("click", () => {
 
 // Adding Contact
 
-const form = document.getElementById("contactForm");
-const contactNumber = document.getElementById("number");
-const nameOfContact = document.getElementById("name");
-const addressOfContact = document.getElementById("address");
-const secondLine = document.getElementById("secondline");
-const studentDom = document.querySelector(".contacts");
+// const form = document.getElementById("contactForm");
+// const contactNumber = document.getElementById("number");
+// const nameOfContact = document.getElementById("name");
+// const addressOfContact = document.getElementById("address");
+// const secondLine = document.getElementById("secondline");
+// const studentDom = document.querySelector(".contacts");
 
-const contact = JSON.parse(localStorage.getItem("contact")) || [];
+// const contact = JSON.parse(localStorage.getItem("contact")) || [];
 
-const createContact = ({ person, address, line, otherLine }) => {
-  const container = document.createElement("ul");
+// const createContact = ({ person, address, line, otherLine }) => {
+//   const container = document.createElement("ul");
 
-  const contactName = document.createElement("li");
-  const contactAddress = document.createElement("li");
-  const personNumber = document.createElement("li");
-  const contactSecondLine = document.createElement("li");
+//   const contactName = document.createElement("li");
+//   const contactAddress = document.createElement("li");
+//   const personNumber = document.createElement("li");
+//   const contactSecondLine = document.createElement("li");
 
-  container.classList.add("list-items");
-  contactName.classList.add("items");
-  contactAddress.classList.add("items");
-  personNumber.classList.add("items");
-  contactSecondLine.classList.add("items");
+//   container.classList.add("list-items");
+//   contactName.classList.add("items");
+//   contactAddress.classList.add("items");
+//   personNumber.classList.add("items");
+//   contactSecondLine.classList.add("items");
 
-  contactName.innerHTML = `Name : ${person}`;
-  contactAddress.innerHTML =`Name : ${address}`;
-  personNumber.innerHTML = `Phone Number : ${ line  >= 0 ? line : ""}`;
-  contactSecondLine.innerHTML = ` Other line : ${otherLine >= 0 ? otherLine : ""}` ;
+//   contactName.innerHTML = `Name : ${person}`;
+//   contactAddress.innerHTML =`Name : ${address}`;
+//   personNumber.innerHTML = `Phone Number : ${ line  >= 0 ? line : ""}`;
+//   contactSecondLine.innerHTML = ` Other line : ${otherLine >= 0 ? otherLine : ""}` ;
 
-  container.append(
-    contactName,
-    contactAddress,
-    personNumber,
-    contactSecondLine
-  );
+//   container.append(
+//     contactName,
+//     contactAddress,
+//     personNumber,
+//     contactSecondLine
+//   );
      
-  studentDom.appendChild(container);
-};
+//   studentDom.appendChild(container);
+// };
 
 const addContact = (person, address, line, otherLine) => {
   contact.push({ person, address, line, otherLine });
