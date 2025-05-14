2c1dad44b319dbccf64c59d3453c58f9658417fe
TimiMaliki <malikitimi@gmail.com>
2025-05-14 22:12:16 +0100

    REM-PX-CONVERTER

---
+++ FrontEnd-portfolio/JavaScript/REM-PX-Converter/src/index.js
@@ -0,0 +1,40 @@
+const remValue = document.querySelector(".rem");
+const pxValue = document.querySelector(".px");
+const remValueInput = document.querySelector(".remNum");
+const pxValueInput = document.querySelector(".pxNum");
+const button = document.querySelector(".btn");
+const btn = document.querySelector('.btn-Two')
+const resetBtn = document.querySelector(".reset-btn");
+
+
+
+
+button.addEventListener("click", (e) => {
+    remValueField()
+    remValue.innerHTML = `${remValueInput.value}rem`
+})
+
+btn.addEventListener("click", (e) => {
+    pxValueField()
+    pxValue.innerHTML =  `${pxValueInput.value}px`
+})
+
+
+resetBtn.addEventListener("click", (e) => {
+    pxValue.innerHTML = ""
+    remValue.innerHTML = ""
+    remValueInput.value = ""
+    pxValueInput.value  = ""
+})
+
+function remValueField(){
+
+    pxValue.innerHTML = `${remValueInput.value * 16 } px` 
+ 
+}
+
+
+function pxValueField(){
+        remValue.innerHTML = `${pxValueInput.value / 16} rem`
+        
+}
