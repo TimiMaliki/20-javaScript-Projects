899faf45eae89ae041380f073f61aa64403a38d1
TimiMaliki <malikitimi@gmail.com>
2025-05-17 18:16:39 +0100

    GSAP PROJECTS -added : timeline

--- FrontEnd-portfolio/GSAP-Animation/GSAP-Tuts-Udemy/Resources/2.Timelines/StartingExamples/script.js
+++ FrontEnd-portfolio/GSAP-Animation/GSAP-Tuts-Udemy/Resources/2.Timelines/StartingExamples/script.js
@@ -7,6 +7,24 @@ const orangeBox  = document.querySelector('.orangeBox');
 const redBox = document.querySelector('.redBox');
 const purpleBox = document.querySelector('.purpleBox');
 
+const tl = gsap.timeline()
+ tl.to(blueBox , {
+    duration : 1 ,
+    y : 100
+ })
+
+ tl.to(greenBox , {
+    duration : 1 ,
+    y: -100,
+ })
+ tl.to(orangeBox, {
+    duration : 1 ,
+    scale : 1.5
+ })
+ tl.to(redBox, {
+    duration : 1 ,
+    rotate: 100
+ })
 
 // The Position Parameter
 const yellowCircle = document.querySelector('.yellowCircle ');
