49d750fd059a8cc460137e7cc8401add728ff794
TimiMaliki <malikitimi@gmail.com>
2025-05-17 17:11:44 +0100

    GSAP PROJECTS -added : to() method

--- FrontEnd-portfolio/GSAP-Animation/GSAP-Tuts-Udemy/Resources/1.Basics of Greensock/StartingExamples/script.js
+++ FrontEnd-portfolio/GSAP-Animation/GSAP-Tuts-Udemy/Resources/1.Basics of Greensock/StartingExamples/script.js
@@ -14,11 +14,25 @@ gsap.to(greenSquare,{
 
 // gsap.from()
 const orangeSquare = document.querySelector('.orangeSquare');
-
+gsap.from(orangeSquare,{
+    duration:2,
+    y:500,
+    opacity:0,
+    scale:0.5,
+    rotation:-90,
+    ease:'bounce.out'
+})
 
 // gsap.to()
 const purpleSquare = document.querySelector('.purpleSquare');
-
+gsap.to(purpleSquare,{
+    duration:2,
+    x:500,
+    rotation:360,
+    borderRadius: "50%",
+    background:"green",
+    ease:"bounce-out"
+})
 
 // gsap.fromTo()
 const blueSquare = document.querySelector('.blueSquare');
