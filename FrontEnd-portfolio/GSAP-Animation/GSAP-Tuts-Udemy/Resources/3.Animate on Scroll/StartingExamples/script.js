755f251b169f2d69c9840a891361f06bebb29cfb
TimiMaliki <malikitimi@gmail.com>
2025-05-18 19:49:57 +0100

    GSAP PROJECTS -added : timeline

--- FrontEnd-portfolio/GSAP-Animation/GSAP-Tuts-Udemy/Resources/3.Animate on Scroll/StartingExamples/script.js
+++ FrontEnd-portfolio/GSAP-Animation/GSAP-Tuts-Udemy/Resources/3.Animate on Scroll/StartingExamples/script.js
@@ -6,7 +6,15 @@ gsap.registerPlugin(ScrollTrigger);
 const navySquare = document.querySelector('.navySquare');
 const sectionTitle = document.querySelector('h1');
 
-
+gsap.to(navySquare ,{
+    duration : 2,
+    x : 300, 
+    y: 500,
+    rotate: 90,
+    borderRadius : "50%",
+    scale : 4.9,
+    scrollTrigger :  sectionTitle
+})
 
 // ScrollTrigger Basic Implementation
 // #f3a712
