861f6e7e923ce65e0628744bafc9ae0cb67990a9
TimiMaliki <malikitimi@gmail.com>
2025-05-30 11:06:59 +0100

    js-promises

---
+++ FrontEnd-portfolio/CallBacks,Promises,Async-Await,Js-Fetch-API/src/index.js
@@ -0,0 +1,20 @@
+// const myPromise = new Promise((resolve,reject) =>{
+//          const error = false
+//          if(!error){
+//             resolve("yes, it resolved")
+//          }else{
+//             reject("no, rejected")
+//          }
+// })
+
+// console.log(myPromise)
+
+
+
+const validate = true
+
+if(!validate){
+    console.log("true")
+}else{
+    console.log("false")
+}
